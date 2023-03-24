af504371e8267f85cf38f75cb82804ea482eb332
grmbyrn <graemebyrne_89@hotmail.com>
2023-03-24 19:34:15 +0100

    install and use jsonwebtokens

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from 'bcryptjs';
-
+import jwt from "jsonwebtoken";
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -23,6 +23,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide password'],
         minLength: 6,
+        select: false
     },
     lastName: {
         type: String,
@@ -42,5 +43,11 @@ UserSchema.pre('save', async function(){
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
 })
+
+UserSchema.methods.createJWT = function(){
+    return jwt.sign({userId: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME
+    })
+}
 
 export default mongoose.model('User', UserSchema)
