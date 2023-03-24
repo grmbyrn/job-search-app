80c03f1c13bf597f8021943b5acd37559f7237f3
grmbyrn <graemebyrne_89@hotmail.com>
2023-03-24 16:01:51 +0100

    add error handling for bad request error, not found errors, and empty values, missing field, etc errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,16 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes";
+import {BadRequestError} from '../errors/index.js'
 
 const register = async (req, res) => {
-        const user = await User.create(req.body);
-        res.status(StatusCodes.OK).json({ user });
+    const {name, email, password} = req.body
+
+    if(!name || !email ||!password){
+        throw new BadRequestError('Please provide all values')
+    }
+
+    const user = await User.create({name, email, password});
+    res.status(StatusCodes.OK).json({ user });
 }
 
 const login = (req, res) => {
